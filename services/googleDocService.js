62aeebfc01cf84cfb60cc172e4e916983bdc5147
Muhitori <39372404+Muhitori@users.noreply.github.com>
2024-04-18 14:51:16 +0300

    resize, dropdown, new googledoc algorithm

--- services/googleDocService.js
+++ services/googleDocService.js
@@ -6,6 +6,7 @@ import {
 	GOOGLE_PRIVATE_KEY,
 } from "../constants/environment.js";
 import { getSheetDate } from "../utils/time.js";
+import axios from "axios";
 
 const serviceAccountAuth = new JWT({
 	email: GOOGLE_SERVICE_ACCOUNT_EMAIL,
@@ -25,6 +26,8 @@ const DATE_COLUMN = "Дата отгрузки";
 const PRICE_COLUMN = "Цена";
 const STATUS_COLUMN = "Статус";
 const POSTING_NUMBER_COLUMN = "Номер заказа";
+
+const DROPDOWN_VALUES = ["Ожидание ответа", "Подтверждён", "На базе", "Отмена"];
 
 const getColor = (color) => {
 	return COLOR_MAX - color;
@@ -60,6 +63,93 @@ const prepareHeader = async (sheet) => {
 	);
 };
 
+const batchUpdate = async (data) => {
+	const response = await axios.post(
+		`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}:batchUpdate`,
+		data,
+		{
+			headers: {
+				Authorization: `Bearer ${serviceAccountAuth.gtoken.accessToken}`,
+				"Content-Type": "application/json",
+			},
+		}
+	);
+
+	return response;
+};
+
+const createDropdown = async (sheet, sheetId, count) => {
+	const request = {
+		requests: [
+			{
+				setDataValidation: {
+					range: {
+						sheetId: sheetId,
+						startRowIndex: 1,
+						endRowIndex: count + 1,
+						startColumnIndex: 5,
+						endColumnIndex: 6,
+					},
+					rule: {
+						condition: {
+							type: "ONE_OF_LIST",
+							values: DROPDOWN_VALUES.map((value) => ({
+								userEnteredValue: value,
+							})),
+						},
+						strict: true,
+					},
+				},
+			},
+		],
+	};
+
+	try {
+		await batchUpdate(request);
+
+		for (let i = 1; i <= count; i++) {
+			const cell = sheet.getCell(i, 5);
+
+			if (!cell.value) {
+				cell.value = DROPDOWN_VALUES[0];
+			}
+		}
+
+		await sheet.saveUpdatedCells();
+	} catch (err) {
+		console.error(
+			"Error creating dropdown:",
+			err.response ? err.response.data.error : err.message
+		);
+	}
+};
+
+const autoResize = async (sheetId) => {
+	const resizeRequest = {
+		requests: [
+			{
+				autoResizeDimensions: {
+					dimensions: {
+						sheetId: sheetId,
+						dimension: "COLUMNS",
+						startIndex: 0,
+						endIndex: 8,
+					},
+				},
+			},
+		],
+	};
+
+	try {
+		await batchUpdate(resizeRequest);
+	} catch (err) {
+		console.error(
+			"Error:",
+			err.response ? err.response.data.error : err.message
+		);
+	}
+};
+
 export class GoogleDocService {
 	static async updateSheet(data) {
 		try {
@@ -73,50 +163,48 @@ export class GoogleDocService {
 				sheet = await doc.addSheet({ title: sheetDate });
 			}
 
-			await sheet.clear(`A1:C400`);
-			await sheet.clear(`G1:G400`);
 			await sheet.loadCells(RANGE);
-
-			for (let i = 1; i < ROWS; i++) {
-				for (let j = 0; j < COLUMNS; j++) {
-					const cell = sheet.getCell(i, j);
-					cell.clearAllFormatting();
-				}
-			}
-			await sheet.saveUpdatedCells();
 
 			prepareHeader(sheet);
 
+			const rows = await sheet.getRows();
+			const rowsToAdd = [];
+
 			for (let i = 0; i < data.length; i++) {
-				for (let j = 0; j < COLUMNS; j++) {
-					const cell = sheet.getCell(i + 1, j);
+				const newRow = {
+					[NUMBER_COLUMN]: i + 1,
+					[NAME_COLUMN]: data[i].name,
+					[DATE_COLUMN]: data[i].shipmentDate,
+					[POSTING_NUMBER_COLUMN]: data[i].postingNumber,
+				};
 
-					if (j === 0) {
-						cell.value = i + 1;
-					}
+				const index = rows.findIndex((row) => {
+					const oldRow = row.toObject();
 
-					if (j === 1) {
-						const { red, green, blue } = data[i].color;
-						cell.value = data[i].name;
+					return (
+						newRow[NAME_COLUMN] === oldRow[NAME_COLUMN] &&
+						newRow[POSTING_NUMBER_COLUMN] === oldRow[POSTING_NUMBER_COLUMN]
+					);
+				});
 
-						cell.backgroundColor = {
-							red: getColor(red),
-							green: getColor(green),
-							blue: getColor(blue),
-						};
-					}
+				if (index === -1) {
+					rowsToAdd.push(newRow);
+					const cell = sheet.getCell(i + 1, 1);
 
-					if (j === 2) {
-						cell.value = data[i].shipmentDate;
-					}
+					const { red, green, blue } = data[i].color;
 
-					if (j === 6) {
-						cell.value = data[i].postingNumber;
-					}
+					cell.backgroundColor = {
+						red: getColor(red),
+						green: getColor(green),
+						blue: getColor(blue),
+					};
 				}
 			}
 
+			await sheet.addRows(rowsToAdd);
 			await sheet.saveUpdatedCells();
+			await createDropdown(sheet, sheet.sheetId, data.length);
+			await autoResize(sheet.sheetId);
 
 			return sheet.sheetId;
 		} catch (err) {
